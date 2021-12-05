c22ee6708139b5afb807e5f1fa66c1363b1c0516
Hannan Fakhrul Hakim <58686484+hannanfh@users.noreply.github.com>
2021-12-05 22:58:17 +0700

    Menyelesaikan video ke 3 JSON peretemuan ke 11

--- latihan1.php
+++ latihan1.php
@@ -1,21 +1,21 @@
 <?php
 
 $mahasiswa =
-  //  [
-  // // //   [
-  // // //     "name" => "Hannan Fakhrul Hakim",
-  // // //     "nrp" => "193040073",
-  // // //     "email" => "193040073@mail.unpas.ac.id"
-  // // //   ],
-  // // //   [
-  // // //     "name" => "Dadang Turbo",
-  // // //     "nrp" => "193040070",
-  // // //     "email" => "193040070@mail.unpas.ac.id"
-  // // //   ]
-  // // // ];
+  [
+    [
+      "name" => "Hannan Fakhrul Hakim",
+      "nrp" => "193040073",
+      "email" => "193040073@mail.unpas.ac.id"
+    ],
+    [
+      "name" => "Dadang Turbo",
+      "nrp" => "193040070",
+      "email" => "193040070@mail.unpas.ac.id"
+    ]
+  ];
 
 
-  $dbh = new PDO('mysql:host=localhost;dbname=phpdasar', 'root', '');
+$dbh = new PDO('mysql:host=localhost;dbname=phpdasar', 'root', '');
 $db = $dbh->prepare('SELECT * FROM mahasiswa');
 $db->execute();
 $mahasiswa = $db->fetchAll(PDO::FETCH_ASSOC);
